c1140a10a874cef5481440e17c93a6e4d9e7246f
flazzing <limchengqing@gmail.com>
2021-03-06 19:45:02 -0800

    added routing, navbar, and basic implementation of hook for testlocation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+/** @jsxImportSource @emotion/react */
+
+import React from "react";
+import { css } from "@emotion/react";
+import useTestCenters from "./hooks/useTestCenters";
+import Navbar from "./components/navbar";
+import { Switch, Route, Redirect, Link } from "react-router-dom";
+
+const backgroundColor = css`
+	background-color: #000000;
+	${"" /* height: 200px; */}
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const { testLocations, isLoading } = useTestCenters("AK");
+	// console.log(testLocations);
+
+	// const { globalTracker, isGlobalLoading } = useGlobalTracker();
+
+	return (
+		<div css={backgroundColor}>
+			<Navbar />
+
+			<Switch>
+				<Route path="/home"></Route>
+				<Route path="/statistic/us"></Route>
+				<Route path="/statistic/us/:state"></Route>
+				<Route path="/statistic/us/:states/hospitalizations"></Route>
+				<Route path="/statistic/us/:states/deaths"></Route>
+				<Route path="/testlocation"></Route>
+				<Route exact path="/"></Route>
+			</Switch>
+		</div>
+	);
 }
 
 export default App;
